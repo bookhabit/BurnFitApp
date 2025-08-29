56bc2828a79eb4b2c7dce487bbc49ff4ce0c7393
이현진 <qnrgoqlt@naver.com>
2025-08-29 10:33:17 +0900

    feat : 프로젝트 세팅

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const HomeTabScreen = () => {
+  return (
+    <View>
+      <Text>HomeTabScreen</Text>
+    </View>
+  )
+}
+
+export default HomeTabScreen
